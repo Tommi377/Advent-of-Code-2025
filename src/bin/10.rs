39deb5cf5e47c27925efa0137c09940b052afc6c
Tommi Pakarinen <tommivinhpakarinen@gmail.com>
2025-12-10 22:35:03 +0200

    day 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,52 +1,125 @@
-use std::{default, fs};
+use std::{
+    collections::{HashMap, LinkedList},
+    fs,
+};
 
-use itertools::Itertools;
+use z3::{Optimize, ast::Int};
+
+#[derive(Clone, Default, Debug)]
+struct MachineInput {
+    lights: Vec<bool>,
+    buttons: Vec<Vec<u16>>,
+    joltages: Vec<u16>,
+}
 
 fn main() {
-    let input = fs::read_to_string("data/example/10").expect("File not found");
+    let input = fs::read_to_string("data/input/10").expect("File not found");
     println!("{}", part1(&input));
-    // println!("{}", part2(&input));
+    println!("{}", part2(&input));
 }
 
 fn part1(input: &str) -> u64 {
-    let map = parse_input(input);
-
-    let mut largest: u64 = 0;
-    for (i, &point_a) in map.iter().enumerate() {
-        for &point_b in map.iter().skip(i + 1) {
-            let area = (point_a.0.abs_diff(point_b.0) + 1) * (point_a.1.abs_diff(point_b.1) + 1);
-            if largest < area {
-                largest = area;
-            }
-        }
-    }
-
-    largest
+    parse_input(input).iter().map(solve_light).sum()
 }
 
 fn part2(input: &str) -> u64 {
-    todo!()
+    parse_input(input).iter().map(solve_joltage).sum()
 }
 
-#[derive(Clone, Default)]
-struct MachineInput {
-    lights: Vec<bool>,
-    buttons: Vec<u8>,
-    joltages: Vec<u8>,
+fn solve_light(machine: &MachineInput) -> u64 {
+    let mut queue: LinkedList<Vec<bool>> = LinkedList::new();
+    let mut came_from: HashMap<Vec<bool>, Vec<bool>> = HashMap::new();
+
+    let start_state = vec![false; machine.lights.len()];
+    queue.push_back(start_state.clone());
+    while let Some(state) = queue.pop_front() {
+        for button in &machine.buttons {
+            let new_state = apply_button_light(&state, button);
+
+            if new_state == machine.lights {
+                let mut path_length = 1;
+                let mut current_state = state;
+                while let Some(prev_state) = came_from.get(&current_state) {
+                    path_length += 1;
+                    current_state = prev_state.clone();
+                    if current_state == start_state {
+                        break;
+                    }
+                }
+                return path_length;
+            }
+
+            if came_from.contains_key(&new_state) {
+                continue;
+            }
+
+            came_from.insert(new_state.clone(), state.clone());
+            queue.push_back(new_state);
+        }
+    }
+    unreachable!()
+}
+
+fn solve_joltage(machine: &MachineInput) -> u64 {
+    let solver = Optimize::new();
+
+    let button_frees = machine
+        .buttons
+        .iter()
+        .enumerate()
+        .map(|(button_idx, _)| Int::new_const(format!("{button_idx}")))
+        .collect::<Vec<_>>();
+
+    button_frees
+        .iter()
+        .for_each(|button_free| solver.assert(&button_free.ge(0)));
+
+    for joltage_idx in 0..machine.joltages.len() {
+        let sum_expr = machine
+            .buttons
+            .iter()
+            .enumerate()
+            .filter(|(_, button)| button.contains(&(joltage_idx as u16)))
+            .map(|(button_idx, _)| Int::new_const(format!("{button_idx}")))
+            .fold(Int::from_u64(0), |acc, expr| acc + expr);
+
+        let value = machine.joltages[joltage_idx];
+        solver.assert(&sum_expr.eq(value));
+    }
+
+    solver.minimize(
+        &button_frees
+            .iter()
+            .fold(Int::from_u64(0), |acc, expr| acc + expr),
+    );
+    solver.check(&[]);
+    let model = solver.get_model().unwrap();
+    machine
+        .buttons
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            model
+                .eval(&button_frees[i], true)
+                .unwrap()
+                .as_u64()
+                .unwrap()
+        })
+        .sum()
 }
 
 fn parse_input(input: &str) -> Vec<MachineInput> {
-    let line_count = input.lines().count();
     let mut machines = Vec::new();
     for line in input.lines() {
         let mut machine = MachineInput::default();
-        for (i, token) in line.split_whitespace().enumerate() {
+        let mut it = line.split_whitespace().enumerate().peekable();
+        while let Some((i, token)) = it.next() {
             if i == 0 {
                 machine.lights = parse_lights(token)
-            } else if i == line_count - 1 {
-                machine.buttons = parse_joltages(token)
+            } else if it.peek().is_none() {
+                machine.joltages = parse_comma_separated(token)
             } else {
-                machine.joltages = parse_button(token)
+                machine.buttons.push(parse_comma_separated(token))
             }
         }
         machines.push(machine);
@@ -62,16 +135,26 @@ fn parse_lights(token: &str) -> Vec<bool> {
         .collect()
 }
 
-fn parse_button(token: &str) -> Vec<u8> {
+fn parse_comma_separated(token: &str) -> Vec<u16> {
     token
         .split(',')
-        .map(|s| s.chars().filter(|&c| c.is_digit(10)).collect::<String>())
-        .map(|s| s.parse::<u8>().unwrap())
+        .map(|s| {
+            s.chars()
+                .filter(|&c| c.is_ascii_digit())
+                .map(|c| ((c as u8) - b'0') as u16)
+                .fold(0, |acc, d| acc * 10 + d)
+        })
         .collect()
 }
 
-fn parse_joltages(token: &str) -> Vec<u8> {
-    todo!()
+fn apply_button_light(state: &[bool], button: &Vec<u16>) -> Vec<bool> {
+    let mut new_state = Vec::from(state);
+    for &index in button {
+        if let Some(light) = new_state.get_mut(index as usize) {
+            *light = !*light;
+        }
+    }
+    new_state
 }
 
 #[cfg(test)]
@@ -83,21 +166,21 @@ mod test_day10 {
 
     #[test]
     fn test_part1_example() {
-        assert_eq!(part1(EXAMPLE_INPUT), 50);
+        assert_eq!(part1(EXAMPLE_INPUT), 7);
     }
 
     #[test]
     fn test_part1_input() {
-        assert_eq!(part1(INPUT), 4755278336);
+        assert_eq!(part1(INPUT), 509);
     }
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(EXAMPLE_INPUT), 24);
+        assert_eq!(part2(EXAMPLE_INPUT), 33);
     }
 
     #[test]
     fn test_part2_input() {
-        assert_eq!(part2(INPUT), 1534043700);
+        assert_eq!(part2(INPUT), 20083);
     }
 }
